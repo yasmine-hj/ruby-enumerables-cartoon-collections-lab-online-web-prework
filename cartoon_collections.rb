9d212c8f313454ff699cc5b27863c374efd27ed0
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-05 19:48:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (dwarves)
+  
+  dwarves = ["Dopey", "Grumpy", "Bashful"]
+  
+  dwarves.each_with_index do |dwarves, index|
+    
+    puts "#{index+1}.#{dwarves}" 
+    
+  end
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  
+  veggies.map { |call| call.capitalize + '!' }
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  
 end
 
 def find_the_cheese# code an argument here
